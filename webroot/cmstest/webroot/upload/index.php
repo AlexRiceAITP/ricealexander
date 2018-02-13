4ed8d51e7b928e5cf27fd85b6c17ac295e11bd06
Alex Rice <Heliosanctus@gmail.com>
2018-02-13 13:21:13 -0600

    more work on Upoad
    - error messages
    - table format
    - working on image integration

--- webroot/cmstest/webroot/upload/index.php
+++ webroot/cmstest/webroot/upload/index.php
@@ -16,13 +16,13 @@
   $pathYear = $_SERVER['DOCUMENT_ROOT'].'/submissions/'.$semester.'/';
 
   if (!file_exists($pathYear)) mkdir($pathYear);
-  
+
   // VALIDATE
   $error = "";
   
-  if ($subTitle == null) {
-    $error = "title field is required<br>";
-  } else {
+  if ($subTitle == null)
+	$error .= "title field is required<br>";
+  else {
 	
 	///  submission title should be lowercase with underscores in place of spaces
 	///  create a path to a store the new file
@@ -30,8 +30,12 @@
     $subPath = $pathYear.$pathTitle.'/';
 
 	if (file_exists($subPath))
-      $error = "a submission already exists with that title";
+      $error .= "a submission already exists with that title<br>";
   }
+  
+  if ($subAuth == null)
+	$error .= "author field is required<br>";
+  
   echo $error;
   /*
   if(isset($_POST['submission'])){
@@ -56,13 +60,31 @@
   }*/
 ?>
 <h2>Upload</h2>
-<form action="." method="post">
-Title: <input type="text" name="title"><br>
-Author: <input type="text" name="author"><br>
-Submission
-<textarea name="submission">
+<form action="." method="post" enctype="multipart/form-data">
+<h2>Submission Information</h2>
 
-</textarea>
+<table>
+  <tr>
+    <td>Title:</td><td><input type="text" name="title"></td>
+  </tr><tr>
+    <td>Author:</td><td><input type="text" name="author"></td>
+  </tr><tr>
+    <td>Submission Type:</td>
+	<td><select name="subtype">
+       <option value="text-0" selected>Writing</option>
+       <option value="text-1">Poetry</option>
+       <option value="text-2">Serials</option>
+       <option value="file-0">Visual Art</option>
+       <option value="file-1">Photography</option>
+       <option value="file-2">Music</option>
+     </select></td>
+  </tr><tr>
+  <td>Submission File:</td>
+  <td><input type="file" name="fileToUpload" id="fileToUpload"></td>
+  </tr><tr>
+  <td>Submission Text:</td>
+  <td><textarea name="submission"></textarea></td>
+</table>
 <input type="submit" value="Save" class="modal_button button-save">
 </form>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php';?>
