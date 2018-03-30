b3044e38a60710d39984b73d6ba8839776d758e0
Alex Rice <Heliosanctus@gmail.com>
2018-03-29 19:02:53 -0500

    Animated Patterns Revamp
    Overhauled Rain.js
    - incorporated a random seed function
    - incorporated a refresh function
    - rain function now takes seed as an argument
    - refresh button now utilizes the proper function
    - added styling for refresh button
    - minor spacing edits
    - var to let

--- webroot/patterns/animated/rain.js
+++ webroot/patterns/animated/rain.js
@@ -1,15 +1,29 @@
-if (typeof seed !== 'undefined') rain();
+var repeat;
 
-function rain() {
+function rain(seed) {
   let pos = window.getComputedStyle(document.body).getPropertyValue('background-position').split(/[\s,]+/);
   let bgpos = "";
   
-  for (var i = 0; i < pos.length; i++) {
+  for (let i = 0; i < pos.length; i++) {
     let posi = parseInt(pos[i]) + seed[i];
 	if ((i % 2 != 0) && (i != pos.length - 1)) bgpos += posi + "px, "
 	else bgpos += posi + "px ";
   }
 
   document.body.style.backgroundPosition = bgpos;
-  setTimeout(() => rain(), 50);
+  repeat = setTimeout(() => rain(seed), 50);
+}
+
+function randomSeed(tot) {
+  let seed = [];
+  for (let i = 0; i < tot; i++) {
+    if ((i == tot-1) || (i == tot-2)) seed[i] = 0;
+    else seed[i] = Math.floor((Math.random()*5))-2;
+  }
+  return seed;
+}
+
+function refresh(tot) {
+  clearTimeout(repeat);
+  rain(randomSeed(tot));
 }
