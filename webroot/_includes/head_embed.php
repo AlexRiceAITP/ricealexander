afd287612065e517e225e4c2f453d20812a49d7f
Alex Rice <Heliosanctus@gmail.com>
2018-12-27 17:54:43 -0600

    Create head_embed.php

---
+++ webroot/_includes/head_embed.php
@@ -0,0 +1,24 @@
+<?php /* Google Analytics Code */ ?>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115101179-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-115101179-1');
+  </script>
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/favicon.ico">
+
+<?php /* Favicon Code - https://realfavicongenerator.net/ */ ?>
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/assets/favicons/site.webmanifest">
+  <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#0063a6">
+  <link rel="shortcut icon" href="/assets/favicons/favicon.ico">
+  <meta name="msapplication-TileColor" content="#0063a6">
+  <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#f1fafe">
